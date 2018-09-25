1c4c1ae7a5fc08e97dc9d2a096a2c38748a906bb
bleach86 <josh.leach86@yahoo.com>
2018-09-25 01:58:17 -0500

    fix typo

--- algo/alliumV2.c
+++ algo/alliumV2.c
@@ -15,36 +15,36 @@ void alliumV2_hash(void *state, const void *input)
 	uint32_t _ALIGN(128) hashA[8], hashB[8];
 
 	sph_blake256_context     ctx_blake;
-	    sph_keccak256_context    ctx_keccak;
-	    sph_cubehash512_context  ctx_cubehash;
-	    sph_skein256_context     ctx_skein;
-	    sph_groestl256_context   ctx_groestl;
+	sph_keccak256_context    ctx_keccak;
+	sph_cubehash256_context  ctx_cubehash;
+	sph_skein256_context     ctx_skein;
+	sph_groestl256_context   ctx_groestl;
 
-	    sph_blake256_init(&ctx_blake);
-	    sph_blake256(&ctx_blake, input, 80);
-	    sph_blake256_close(&ctx_blake, hashA);
+	sph_blake256_init(&ctx_blake);
+	sph_blake256(&ctx_blake, input, 80);
+	sph_blake256_close(&ctx_blake, hashA);
 
-	    sph_keccak256_init(&ctx_keccak);
-	    sph_keccak256(&ctx_keccak, hashA, 32);
-	    sph_keccak256_close(&ctx_keccak, hashB);
+	sph_keccak256_init(&ctx_keccak);
+	sph_keccak256(&ctx_keccak, hashA, 32);
+	sph_keccak256_close(&ctx_keccak, hashB);
 
-	    LYRA2(hashA, 32, hashB, 32, hashB, 32, 1, 4, 4);
+	LYRA2(hashA, 32, hashB, 32, hashB, 32, 1, 4, 4);
 
-	    sph_cubehash256_init(&ctx_cubehash);
-	    sph_cubehash256(&ctx_cubehash, hashA, 32);
-	    sph_cubehash256_close(&ctx_cubehash, hashB);
+	sph_cubehash256_init(&ctx_cubehash);
+	sph_cubehash256(&ctx_cubehash, hashA, 32);
+	sph_cubehash256_close(&ctx_cubehash, hashB);
 
-	    LYRA2(hashA, 32, hashB, 32, hashB, 32, 1, 4, 4);
+	LYRA2(hashA, 32, hashB, 32, hashB, 32, 1, 4, 4);
 
-	    sph_skein256_init(&ctx_skein);
-	    sph_skein256(&ctx_skein, hashA, 32);
-	    sph_skein256_close(&ctx_skein, hashB);
+	sph_skein256_init(&ctx_skein);
+	sph_skein256(&ctx_skein, hashA, 32);
+	sph_skein256_close(&ctx_skein, hashB);
 
-	    sph_groestl256_init(&ctx_groestl);
-	    sph_groestl256(&ctx_groestl, hashB, 32);
-	    sph_groestl256_close(&ctx_groestl, hashA);
+	sph_groestl256_init(&ctx_groestl);
+	sph_groestl256(&ctx_groestl, hashB, 32);
+	sph_groestl256_close(&ctx_groestl, hashA);
 
-	    memcpy(state, hashA, 32);
+	memcpy(state, hashA, 32);
 }
 
 int scanhash_alliumV2(int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done)
